d9e38fcb410888da678d055aba12fbe85cbf385d
guox18 <104893176@qq.com>
2026-01-21 19:15:12 +0800

    use option + up/down to move lines

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,11 @@ vim.keymap.set({ "n", "x" }, "s", "s", { desc = "Substitute" })
 
 -- 恢复 S 的原始功能（删除整行并进入插入模式）
 vim.keymap.set("n", "S", "S", { desc = "Substitute line" })
+
+-- 用 Option + 上下箭头 移动行（替代 Option + j/k）
+vim.keymap.set("n", "<A-Up>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+vim.keymap.set("n", "<A-Down>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+vim.keymap.set("i", "<A-Up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+vim.keymap.set("i", "<A-Down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+vim.keymap.set("v", "<A-Up>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+vim.keymap.set("v", "<A-Down>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
