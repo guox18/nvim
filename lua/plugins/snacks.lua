68ce4ba89693a588c4a2cb18be130f05e95bfb7b
guox18 <104893176@qq.com>
2026-02-04 13:34:26 +0000

    modify snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,6 +2,10 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    -- 禁用 image 功能，避免在不支持的终端中显示 "SIXEL IMAGE" 提示
+    image = {
+      enabled = false,
+    },
     explorer = {
       replace_netrw = true,
     },
